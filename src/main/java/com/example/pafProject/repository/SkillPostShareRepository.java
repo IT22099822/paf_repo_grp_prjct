c44c93206829a241e54cd103306c98ae481fff3e
Vinuki87 <it22178572@my.sliit.lk>
2025-05-01 15:19:53 +0530

    Added a backend files for skill sharing

---
+++ src/main/java/com/example/pafProject/repository/SkillPostShareRepository.java
@@ -0,0 +1,7 @@
+package com.example.pafProject.repository;
+
+import com.example.pafProject.model.SkillPostShare;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SkillPostShareRepository extends MongoRepository<SkillPostShare, String> {
+}
